a8aa965f1efb82390f5e059d9c6c7c5f19c3eebb
Alexander Glushkov <cutalion@gmail.com>
2026-04-16 20:13:19 +0400

    feat: add Eidos.configure for global SDK configuration

--- eidos/lib/eidos.rb
+++ eidos/lib/eidos.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
 require_relative 'eidos/version'
+require_relative 'eidos/sdk_configuration'
 
 # Eidos - IP World Engine
 # Main entry point for the Eidos gem
 module Eidos
+  class << self
+    def configuration
+      @configuration ||= SdkConfiguration.new
+    end
+
+    def configure
+      yield(configuration)
+    end
+
+    def reset_configuration!
+      @configuration = SdkConfiguration.new
+    end
+  end
 end
